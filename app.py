19f585cfe9873d54fb8137279b1fd14cba91e636
Kristina Myftaraga <knm181@aubg.edu>
2020-05-21 12:38:21 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+import os
+
+
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/caricatures")
+def caricatures():
+    return render_template("caricature.html")
+
+@app.route("/portraits")
+def portraits():
+    return render_template("portrait.html")
+
+@app.route("/logos")
+def logos():
+    return render_template("logos.html")
+
+@app.route("/posters")
+def posters():
+    return render_template("poster.html")
+
+@app.route("/tshirt")
+def tshirt():
+    return render_template("tshirt.html")
+
+
+if __name__ == "__main__":
+    app.debug=True
+    app.run()
